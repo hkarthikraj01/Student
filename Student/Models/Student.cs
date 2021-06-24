e0a01b032c9f98fc61c4644ce1eaa64a885c0b14
KARTHIKRAJ H <hkarthikraj01@gmail.com>
2021-06-24 12:04:03 +0530

    Student  Model

---
+++ Student/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string RegisterNo { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
